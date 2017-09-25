aad1632a48c469e45904908e395d672d77ee4e67
jlyonas <jlyon@acumensolutions.com>
2017-09-25 19:29:13 -0400

    twitter streaming api fail

---
+++ server/twitter-api.js
@@ -0,0 +1,31 @@
+var Twitter = require("twitter");
+var config = require("./keys.js");
+
+var client = new Twitter({
+	consumer_key: config.consumer_key,
+	consumer_secret: config.consumer_secret,
+	access_token_key: config.access_token_key,
+	access_token_secret: config.access_token_secret,
+});
+
+// @conky5, @mrJus71n
+var stream = client.stream("statuses/filter", {track: "javascript"});
+stream.on("data", function(event) {
+	console.log(event && event.text);
+});
+
+var handleStream = function(cb) {
+	stream.on("data", function(event) {
+		//console.log(event && event.text);
+		cb(event);
+	});
+};
+
+stream.on("error", function(error) {
+	throw error;
+});
+
+var twitterApi = {
+	onstream: handleStream,
+};
+module.exports = twitterApi;
